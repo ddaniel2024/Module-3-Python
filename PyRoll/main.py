9d979b4ee560d89907c355b153983586b14bde5b
ddaniel2024 <daniel97daniel@gmail.com>
2024-02-17 13:33:12 +0000

    found a way to calculate votes without pre-entering the votrs array

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -10,16 +10,16 @@ with open(csvpath) as csvfile:
 
     row_count = 0
     candidates=[]
-    votes=[0,0,0]
+    votes=[]
 
     for row in csvreader:
         row_count = row_count + 1
         
         if row[2] not in candidates:
             candidates.append(row[2])
+            votes.append(0)
         
-
-        candidate_index = candidates.index(str(row[2]))
+        candidate_index = candidates.index(row[2])
         votes[int(candidate_index)] +=1
 
     print(f'Total Votes: {row_count}')
