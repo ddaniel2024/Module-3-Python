f21331211197acf1e01e8177226b9e2cbc564eb9
ddaniel2024 <daniel97daniel@gmail.com>
2024-02-17 13:47:28 +0000

    cleaned up code and added percentage of votes

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -8,22 +8,23 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
-    row_count = 0
+    total_votes = 0
     candidates=[]
     votes=[]
 
     for row in csvreader:
-        row_count = row_count + 1
+        total_votes = total_votes + 1
         
         if row[2] not in candidates:
             candidates.append(row[2])
             votes.append(0)
         
-        candidate_index = candidates.index(row[2])
-        votes[int(candidate_index)] +=1
+        candidate_index = int(candidates.index(row[2]))
+        votes[candidate_index] +=1
 
-    print(f'Total Votes: {row_count}')
+    print(f'Total Votes: {total_votes}')
 
-    print(candidates)
-       
-    print(votes)
+    for candidate in candidates:
+        candidate_votes = votes[candidates.index(candidate)]
+        pc_votes = candidate_votes / total_votes
+        print(f'{candidate} {pc_votes} {candidate_votes}')
