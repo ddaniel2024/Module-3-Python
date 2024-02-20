79739207f85422b7a4bcdc8fc69f0aecd8290ab3
ddaniel2024 <daniel97daniel@gmail.com>
2024-02-20 15:37:58 +0000

    cleaned up code by adding a dictionary to PyRoll

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -10,62 +10,56 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
-    #set total to 0, establish blank "candidates" and "total candidate votes" lists (to calculate min and max values later)
+    #set total to 0, establish blank dictionary
     total_votes = 0
-    candidates=[]
-    total_candidate_votes=[]
+    dict = {"Name" : [],
+        "Votes" : [],
+        "pc_vote" : []}
 
-    ##calculate total votes
     #iterate through all the rows
     for row in csvreader:
                 
         #cumulate all the votes to get a total value
         total_votes = total_votes + 1
         
-        #conditional to find unique candidates and append to "candidates" list
-        if row[2] not in candidates:
-            candidates.append(row[2])
-            #for each unique candidate, a "0" is appended in the "total candidate votes" list (to calculate each candidates' votes later)
-            total_candidate_votes.append(0)
-        
-        #each unique candidates' total votes is summed into the "total candidate votes" list
-        candidate_index = int(candidates.index(row[2]))
-        total_candidate_votes[candidate_index] +=1
+        #conditional to find unique candidates and append to dictionary
+        #for each unique candidate, a "0" is appended in the "votes" and "percentage votes" lists
+        if row[2] not in dict["Name"]:
+            dict["Name"].append(row[2])
+            dict["Votes"].append(0)
+            dict["pc_vote"].append(0)
 
-#establish "max votes" list to hold the total votes of each candidate, and set max vote to 0
-max_votes = []
-max_vote = 0
-#establish "results" list to hold results for printing later
-results = []
+        # for each unqiue candidate, add their votes as you iterate through the rows and add to the dictionary
+        for candidate in dict["Name"]:
 
-#iterate through all candidates in "candidates" list and find their total votes by matching their index to the total votes in the "total candidate votes" list
-for candidate in candidates:
-
-    #calculate percentage of votes by dividing the candidate's total vote by the overall total votes. Set to 3 decimal places
-    candidate_votes = total_candidate_votes[candidates.index(candidate)]
-    pc_votes = round(candidate_votes / total_votes *100,3)
-
-    #while iterating, append results to "results" list
-    results.append(f'{candidate}: {pc_votes}% ({candidate_votes})')
+            candidate_index = dict["Name"].index(candidate)
+            
+            if row[2] == candidate:
+                dict["Votes"][candidate_index] +=1
+            
+            #as you iterate through the rows, caluclate the percentage vote for each candidate and add to the dictionary
+            #percentage vote is calculated by dividing the candidate's vote, dividing by the total number of votes, multiplying by 100, then rounding to 3 dp.
+            pc_vote = round(dict["Votes"][candidate_index] / total_votes * 100, 3)
+            dict["pc_vote"][candidate_index] = pc_vote
     
-    #while iterating, each candidate's total vote is appended to the "max votes" list. 
-    max_votes.append(candidate_votes)
-    #the maximum vote value is found and held using this conditional
-    if candidate_votes > max_vote:
-        max_vote = candidate_votes
+#winning candidate is found by using the index of the maximum vote in the "votes" list and applying it to the "name" list in the dictionary
+max_vote = 0 
+for i in range(len(dict["Votes"])):
+    if dict["Votes"][i] > max_vote:
+        max_vote = dict["Votes"][i]
 
-#winning candidate is found by using the index of the maximum vote value in the "max votes" list and applying it to the "candidates" list
-max_votes_index = int(max_votes.index(max_vote))
+max_vote_index = dict["Votes"].index(max_vote)
+winner = dict["Name"][max_vote_index]
 
 #print results to terminal
 print("Election Results")
 print("-------------------------------------------------")
 print(f'Total Votes: {total_votes}')
 print("-------------------------------------------------")
-for row in results:
-    print(row)
+for i in range(len(dict)):
+    print(f'{dict["Name"][i]}: {dict["pc_vote"][i]}% ({dict["Votes"][i]})')
 print("-------------------------------------------------")
-print(f'Winner: {candidates[max_votes_index]}')
+print(f'Winner: {winner}')
 print("-------------------------------------------------")
 
 ##export results to textfile in "analysis" folder
@@ -74,13 +68,13 @@ text_filepath = os.path.join('C:/Users/danie/Documents/Module-3-Python/Module-3-
 
 #if there is no text file at the location, one is created. If there is already a text file, it is re-written
 with open(text_filepath, "w") as file:
-    
+
     file.write("Election Results\n")
     file.write("-------------------------------------------------\n")
     file.write(f'Total Votes: {total_votes}\n')
     file.write("-------------------------------------------------\n")
-    for row in results:
-        file.write(f'{row}\n')
+    for i in range(len(dict)):
+        file.write(f'{dict["Name"][i]}: {dict["pc_vote"][i]}% ({dict["Votes"][i]})\n')
     file.write("-------------------------------------------------\n")
-    file.write(f'Winner: {candidates[max_votes_index]}\n')
+    file.write(f'Winner: {winner}\n')
     file.write("-------------------------------------------------\n")
